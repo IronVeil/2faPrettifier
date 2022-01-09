db73c9d8e67cfa98f12db1c95ecfef56e63f354c
IronVeil <iron.veil@outlook.com>
2022-01-09 23:21:28 +0000

    Commented code, removes whitespaces from 2FA code and fixed bug

--- prettifier.py
+++ prettifier.py
@@ -1,14 +1,22 @@
-# Prettifier
+# 2 Factor Authentication Code Prettifier
+# Turns a mess of numbers and letters into XXXX XXXX XXXX XXXX
+
+
+# Prettifier subroutine
 def prettify(code):
-    return ' '.join([code[i:i+4] for i in range(0, len(code), 3)]).upper()
+
+    # Removes any spaces
+    code = code.replace(" ", "")
+
+    # Splits the string into parts of 4 and joins them with spaces
+    return ' '.join([code[i:i+4] for i in range(0, len(code), 4)]).upper()
+
 
 # Input
 print("2FA PRETTIFIER\n")
 code = input("Please enter 2FA seed: ")
 
+
 # Output
 print("\nThe new code is:")
 print(prettify(code))
-
-# TODO - auto copy (w/ module detection)
-# TODO - clean up README.md
