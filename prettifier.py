fc5bd16e4daf92760c0cf4b250e871f8197728cb
IronVeil <commit@ironveil.anonaddy.me>
2022-02-27 08:40:40 +0000

    Created basic GTK layout

--- prettifier.py
+++ prettifier.py
@@ -1,41 +1,74 @@
-# 2 Factor Authentication Code Prettifier
-# Turns a mess of numbers and letters into XXXX XXXX XXXX XXXX
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+class MyWindow(Gtk.Window):
+    def __init__(self):
+
+        super().__init__(title="2FA Prettifier")
+        self.set_border_width(10)
+        #self.set_size_request(400,200)
+
+        entry = Gtk.Entry()
+        entry.set_icon_from_icon_name(Gtk.EntryIconPosition.PRIMARY, "input-dialpad-symbolic")
+        submit = Gtk.Button(label="Prettify")
+        output = Gtk.Label(label="XXXX XXXX XXXX XXXX")
+
+        grid = Gtk.Grid()
+        grid.set_column_spacing(10)
+        grid.set_row_spacing(10)
+        grid.add(entry)
+        grid.attach_next_to(submit, entry, Gtk.PositionType.RIGHT, 1, 1)
+        grid.attach(output, 0, 2, 2, 1)
+
+        self.add(grid)
+
+win = MyWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
 
 
-# Tries to import copy module
-try:
-    import pyperclip
-    copy = True
+# OLD CODE
 
-except:
-    print("PYPERCLIP IS NOT INSTALLED, INSTALLING IT THROUGH PIP IS ADVISED")
-    copy = False
+# # 2 Factor Authentication Code Prettifier
+# # Turns a mess of numbers and letters into XXXX XXXX XXXX XXXX
 
 
-# Prettifier subroutine
-def prettify(code):
+# # Tries to import copy module
+# try:
+#     import pyperclip
+#     copy = True
 
-    # Removes any spaces
-    code = code.replace(" ", "")
-
-    # Splits the string into parts of 4 and joins them with spaces
-    return ' '.join([code[i:i+4] for i in range(0, len(code), 4)]).upper()
-
-
-# Input
-print("2FA PRETTIFIER\n")
-code = input("Please enter 2FA seed: ")
+# except:
+#     print("PYPERCLIP IS NOT INSTALLED, INSTALLING IT THROUGH PIP IS ADVISED")
+#     copy = False
 
 
-# Output
-code = prettify(code)
-print("\nThe new code is:")
-print(code)
+# # Prettifier subroutine
+# def prettify(code):
+
+#     # Removes any spaces
+#     code = code.replace(" ", "")
+
+#     # Splits the string into parts of 4 and joins them with spaces
+#     return ' '.join([code[i:i+4] for i in range(0, len(code), 4)]).upper()
 
 
-# Autocopy if possible
-if copy:
-    pyperclip.copy(code)
-    print("\nCode copied to clipboard\n")
-else:
-    print("\nCode not copied to clipboard\n")
+# # Input
+# print("2FA PRETTIFIER\n")
+# code = input("Please enter 2FA seed: ")
+
+
+# # Output
+# code = prettify(code)
+# print("\nThe new code is:")
+# print(code)
+
+
+# # Autocopy if possible
+# if copy:
+#     pyperclip.copy(code)
+#     print("\nCode copied to clipboard\n")
+# else:
+#     print("\nCode not copied to clipboard\n")
